341ebb1e331122c26d44fefd0095da6b4b27227d
3kstr3m3 <planetaidan@gmail.com>
2023-09-17 02:52:56 -0400

    fixed nasty bug with api not calling properly and created function for generating enemies

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -9,28 +9,29 @@ public class EnemySpawner : MonoBehaviour
 {
     private const string URI = "https://api.cohere.ai/v1/generate";
 
+    public string cohereOutput;
+
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public static void GenerateEnemies()
+    public void GenerateEnemies()
     {
         UnityEngine.Debug.Log("generate_enemy");
-        return StartCoroutine(PostData_Coroutine());
+        StartCoroutine(PostData_Coroutine());
     }
 
     IEnumerator PostData_Coroutine()
     {
         UnityEngine.Debug.Log("we are here");
-        using (UnityWebRequest request = UnityWebRequest.Post(URI, "{\"max_tokens\": 300, \"temperature\":2.0, \"prompt\": \"Create 5 enemies for a fantasy RPG each with the following attributes listed on separate lines\r\nName: unique name\r\nHealth: an Integer between 10 and 50\r\nDamage: an Integer between 5 and 20\r\nMagic Damage: an Integer between 5 and 20\r\nDefence: an Integer between 1 to 10\r\nSpecies: Choose from the following; Orc, Goblin, Giant, Elf, Undead, Dragon, Merfolk, Giant Spider\r\nSpecial Ability: Choose from the following; Swiftness, Rage, Poison Spray, Fireball.\"}", "application/json"))
+        using (UnityWebRequest request = UnityWebRequest.Post(URI, "{\"max_tokens\": 300, \"temperature\":2.0, \"prompt\": \"Create 5 enemies for a fantasy RPG each with the following attributes listed on separate lines, Name: unique name, Health: an Integer between 10 and 50, Damage: an Integer between 5 and 20, Magic Damage: an Integer between 5 and 20, Defence: an Integer between 1 to 10, Species: Choose from the following; Orc, Goblin, Giant, Elf, Undead, Dragon, Merfolk, Giant Spider, Special Ability: Choose from the following; Swiftness, Rage, Poison Spray, Fireball.\"}", "application/json"))
         {
             request.SetRequestHeader("accept", "application/json");
             request.SetRequestHeader("authorization", "Bearer yOle6J3A5BHkXpIbS0AWrxnS8E4mPQisFcY6l792");
@@ -45,7 +46,8 @@ public class EnemySpawner : MonoBehaviour
             {
                 UnityEngine.Debug.Log("Form upload complete!");
             }
-            return request.downloadHandler.text
+            cohereOutput = request.downloadHandler.text;
+            UnityEngine.Debug.Log("COHERE: " + cohereOutput);
         }
 
     }
