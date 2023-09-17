671682b6f5f5c80eda14243ef4d72e7fbd0e7f86
3kstr3m3 <planetaidan@gmail.com>
2023-09-17 07:39:20 -0400

    added json parsing for the cohere api calls and implemented the generated stats in combat

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,32 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        Stats.name = "GOOBA";
-        Stats.species = "Goblin";
-        Stats.maxHealth = 25;
-        Stats.health = 25;
-        Stats.physicalAmp = 10;
-        Stats.defense = 5;
-        Stats.magicAmp = 0;
+{   
+
+    public static EnemyInstance currentEnemy;
+
+    public class EnemyInstance {
+        public string name;
+        public string species;
+        public int maxHealth;
+        public int health;
+        public int physicalAmp;
+        public int magicAmp;
+        public int defense;
+        public string skill;
+
+        public EnemyInstance(string eName, string eSpecies, int maxHP, int physAmp, int magAmp, int def, string eSkill) {
+            this.name = eName;
+            this.species = eSpecies;
+            this.maxHealth = maxHP;
+            this.health = maxHP;
+            this.physicalAmp = physAmp;
+            this.magicAmp = magAmp;
+            this.defense = def;
+            this.skill = eSkill;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public static class Stats {
-        public static string name;
-        public static string species;
-        public static int maxHealth;
-        public static int health;
-        public static int physicalAmp;
-        public static int magicAmp;
-        public static int defense;
-    }
+    
 }
--- Assets/Scripts/GameEngine.cs
+++ Assets/Scripts/GameEngine.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 using TMPro;
+using System.Text.Json;
 
 
 public class GameEngine : MonoBehaviour
@@ -33,8 +34,9 @@ public class GameEngine : MonoBehaviour
 
     // game logic
     bool yourTurn = true;
+    bool init = false;
     int delayTimer = 0;
-
+    Enemy.EnemyInstance genEnemy;
 
     // Start is called before the first frame update
     void Start()
@@ -49,49 +51,52 @@ public class GameEngine : MonoBehaviour
 
         // Generates initial enemies
         GenerateEnemies();
-        narrator.text = "Generating enemies...";
-        while (string.IsNullOrEmpty(cohereOutput)) {}
         
-        SlowText(Enemy.Stats.name + " appeared!");
 
     }
 
     // Update is called once per frame
     void Update()
     {  
-        UpdateNarrator();
+        if (init) {
+            UpdateNarrator();
+            
+            // updates all text values on screen
+            enemyName.text = Enemy.currentEnemy.name;
+            playerName.text = Player.Stats.name;
+            eHealthText.text = Enemy.currentEnemy.health + "/" + Enemy.currentEnemy.maxHealth;
+            pHealthText.text = Player.Stats.health + "/" + Player.Stats.getMaxHealth();
+            attackText.text = "ATTACK (" + Player.Stats.getPhysicalAmp() + ")";
+            
+            // hides buttons when no action available
+            attackBox.SetActive(yourTurn && delayTimer == 0);
+            skillBox.SetActive(yourTurn && delayTimer == 0);
+            infoBox.SetActive(yourTurn && delayTimer == 0);
 
-        // updates all text values on screen
-        enemyName.text = Enemy.Stats.name;
-        playerName.text = Player.Stats.name;
-        eHealthText.text = Enemy.Stats.health + "/" + Enemy.Stats.maxHealth;
-        pHealthText.text = Player.Stats.health + "/" + Player.Stats.getMaxHealth();
-        attackText.text = "ATTACK (" + Player.Stats.getPhysicalAmp() + ")";
-        
-        // hides buttons when no action available
-        attackBox.SetActive(yourTurn && delayTimer == 0);
-        skillBox.SetActive(yourTurn && delayTimer == 0);
-        infoBox.SetActive(yourTurn && delayTimer == 0);
-
-        // checks if the text is done writing and if the delay between turns is up
-        if (string.IsNullOrEmpty(narratorFinal)) {
-            if (delayTimer > 0) {
-                delayTimer--;
-            } else if (delayTimer == 0) {
-                if (!yourTurn) {
-                    enemyTurn();
+            // checks if the text is done writing and if the delay between turns is up
+            if (string.IsNullOrEmpty(narratorFinal)) {
+                if (delayTimer > 0) {
+                    delayTimer--;
+                } else if (delayTimer == 0) {
+                    if (!yourTurn) {
+                        enemyTurn();
+                    }
+                    
                 }
                 
             }
-            
+        } else {
+            narrator.text = "Enemies Generating...";
+            attackBox.SetActive(false);
+            skillBox.SetActive(false);
+            infoBox.SetActive(false);
         }
-
 
     }
 
     // triggers on each button press
     void AttackPress() {
-        SlowText(Player.Stats.name + " attacked " + Enemy.Stats.name + " for " + damageEnemy(Player.Stats.getPhysicalAmp()) + " damage.");
+        SlowText(Player.Stats.name + " attacked " + Enemy.currentEnemy.name + " for " + damageEnemy(Player.Stats.getPhysicalAmp()) + " damage.");
         yourTurn = false;
         delayTimer = 50;
     }
@@ -104,8 +109,8 @@ public class GameEngine : MonoBehaviour
 
     // execute during enemy's turn
     void enemyTurn() {
-        if (Enemy.Stats.health > 0) {
-            SlowText(Enemy.Stats.name + " attacked " + Player.Stats.name + " for " + damagePlayer(Enemy.Stats.physicalAmp) + " damage.");
+        if (Enemy.currentEnemy.health > 0) {
+            SlowText(Enemy.currentEnemy.name + " attacked " + Player.Stats.name + " for " + damagePlayer(Enemy.currentEnemy.physicalAmp) + " damage.");
             yourTurn = true;
         } else {
             enemyDeath();
@@ -115,14 +120,17 @@ public class GameEngine : MonoBehaviour
     
     // run on enemy death
     void enemyDeath() {
-        SlowText(Enemy.Stats.name + " died. You won!");
+        SlowText(Enemy.currentEnemy.name + " died. You won!");
+        Enemy.currentEnemy = genEnemy;
+        GenerateEnemies();
+        yourTurn = true;
         // add loot choice here and next enemy spawn, etc.
     }
 
     // calculates, and damages the enemy, also returning the damage dealt after defense
     int damageEnemy(int dmg) {
-        int output = (int)((double)dmg * Math.Pow(DEF_MODIFIER, Enemy.Stats.defense));
-        Enemy.Stats.health -= output;
+        int output = (int)((double)dmg * Math.Pow(DEF_MODIFIER, Enemy.currentEnemy.defense));
+        Enemy.currentEnemy.health -= output;
         return output;
     }
     int damagePlayer(int dmg) {
@@ -160,7 +168,8 @@ public class GameEngine : MonoBehaviour
     IEnumerator PostData_Coroutine()
     {
         UnityEngine.Debug.Log("we are here");
-        using (UnityWebRequest request = UnityWebRequest.Post(URI, "{\"max_tokens\": 300, \"temperature\":2.0, \"prompt\": \"Create 5 enemies for a fantasy RPG each with the following attributes listed on separate lines, Name: unique name, Health: an Integer between 10 and 50, Damage: an Integer between 5 and 20, Magic Damage: an Integer between 5 and 20, Defense: an Integer between 1 to 10, Species: Choose from the following; Orc, Goblin, Giant, Elf, Undead, Dragon, Merfolk, Giant Spider, Special Ability: Choose from the following; Swiftness, Rage, Poison Spray, Fireball.\"}", "application/json"))
+        //using (UnityWebRequest request = UnityWebRequest.Post(URI, "{\"max_tokens\": 300, \"temperature\":2.0, \"prompt\": \"Create 5 enemies for a fantasy RPG each with the following attributes listed on separate lines, Name: unique name, Health: an Integer between 10 and 50, Damage: an Integer between 5 and 20, Magic Damage: an Integer between 5 and 20, Defense: an Integer between 1 to 10, Species: Choose from the following; Orc, Goblin, Giant, Elf, Undead, Dragon, Merfolk, Giant Spider, Special Ability: Choose from the following; Swiftness, Rage, Poison Spray, Fireball.\"}", "application/json"))
+        using (UnityWebRequest request = UnityWebRequest.Post(URI, "{\"max_tokens\": 300, \"temperature\":2.0, \"prompt\": \"Create 1 enemy for a fantasy RPG with the following attributes listed on separate lines, Name: unique name, Health: an Integer between 10 and 50, Damage: an Integer between 5 and 20, Magic Damage: an Integer between 5 and 20, Defense: an Integer between 1 to 10, Species: Choose one from the following - Orc, Goblin, Giant, Elf, Undead, Dragon, Merfolk, Giant Spider, Special Ability: Choose one from the following - Swiftness, Rage, Poison Spray, Fireball, Blade Sharpen. Output these in JSON format.\"}", "application/json"))
         {
             request.SetRequestHeader("accept", "application/json");
             request.SetRequestHeader("authorization", "Bearer yOle6J3A5BHkXpIbS0AWrxnS8E4mPQisFcY6l792");
@@ -175,11 +184,47 @@ public class GameEngine : MonoBehaviour
             {
                 UnityEngine.Debug.Log("Form upload complete!");
             }
-            cohereOutput = request.downloadHandler.text;
+            string dirtyOutput = request.downloadHandler.text; 
+            int first = dirtyOutput.IndexOf("text\":\" ");
+            int last = dirtyOutput.IndexOf("}]");
+            // cleaning output to be readable as an object
+            cohereOutput = dirtyOutput.Substring(first + 8, last - first - 9);
+            cohereOutput = cohereOutput.Replace("\\n","");
+            cohereOutput = cohereOutput.Replace("  \\","");
+            cohereOutput = cohereOutput.Replace("\\","");
+            cohereOutput = cohereOutput.Replace("Special Ability","skill");
+            cohereOutput = cohereOutput.Replace("Magic Damage","magicAmp");
+            cohereOutput = cohereOutput.Replace("Damage","physicalAmp");
+            cohereOutput = cohereOutput.Replace("Name","name");
+            cohereOutput = cohereOutput.Replace("Health","maxHealth");
+            cohereOutput = cohereOutput.Replace("Defense","defense");
+            cohereOutput = cohereOutput.Replace("Species","species");
             UnityEngine.Debug.Log("COHERE: " + cohereOutput);
+            var options = new JsonSerializerOptions { IncludeFields = true };
+            CohereReturn newGen = JsonSerializer.Deserialize<CohereReturn>(cohereOutput);
+            genEnemy = new Enemy.EnemyInstance(newGen.name, newGen.species, newGen.maxHealth, newGen.physicalAmp, newGen.magicAmp, newGen.defense, newGen.skill);
+            UnityEngine.Debug.Log("ENEMY HEALTH: " + genEnemy.health);
+            if (!init) {
+                Enemy.currentEnemy = genEnemy;
+                GenerateEnemies();
+                init = true;
+            }
         }
 
     }
+
+    public class CohereReturn
+    {
+        public string name { get; set; }
+        public int maxHealth { get; set; }
+        public int physicalAmp { get; set; }
+        public int magicAmp { get; set; }
+        public int defense { get; set; }
+        public string species { get; set; }
+        public string skill { get; set; }
+    }
+
+
 
 }
    
